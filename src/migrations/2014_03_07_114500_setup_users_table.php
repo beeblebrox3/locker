350bf23e2ab36ad89bcf085a01a5a535b3e30a7f
Luís Henrique <luish.faria@gmail.com>
2014-03-07 11:55:39 -0300

    add migration users table

---
+++ src/migrations/2014_03_07_114500_setup_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SetupUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+        Schema::create('users', function ($table) {
+            $table->increments('id')->unsigned();
+            $table->string('name', 140);
+            $table->string('email', 120)->unique();
+            $table->string('password', 60);
+            $table->string('confirmation_code', 40)->nullable();
+            $table->dateTime('confirmed')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('users');
+	}
+
+}
