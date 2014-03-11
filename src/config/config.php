0d793071a9cbc7e8aa0aeeaaa7dffc34d79cb56a
Neto Braghetto <Netow93@gmal.com>
2014-03-11 09:02:17 -0300

    Minor config

--- src/config/config.php
+++ src/config/config.php
@@ -13,16 +13,6 @@ return array (
     'user_model' => 'Beeblebrox3\Locker\LockerUser',
 
     /**
-     * the subject of email sended to user confirm his account
-     */
-    'subject_email_confirmation' => 'Please confirm your account',
-
-    /**
-     * the subject of email sended to user when his request new password
-     */
-    'subject_email_reset' => 'Here is your new and temporaly password',
-
-    /**
      * the url of login page. Is used to build the default reset password email.
      * It is optional if you use your own views
      */
